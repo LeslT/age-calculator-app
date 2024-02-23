e291c73cc040e6f5022bf895b993395505c1d0da
LeslT <lesliejailine@hotmail.com>
2024-02-22 19:30:39 -0500

    first changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Day
+  DD
+
+  Month
+  MM
+
+  Year
+  YYYY
+
+  -- years
+  -- months
+  -- days
+    </div>
+  );
+}
+
+export default App;
